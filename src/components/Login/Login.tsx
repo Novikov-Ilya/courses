c54e04466f463e753698e6d7ea5f3e7f48462b76
ilya <mrsmile1988@gmail.com>
2026-06-13 20:41:00 +0300

    feature

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -77,7 +77,7 @@ export const Login = () => {
 
           <p>{dictionary.registerIfNoAccount}
             <Link to={'/registration'}>{dictionary.linkRegistration}</Link>
-            
+            <p>new into</p>
           </p>
         </SimpleFormStyled>
       </WrapperStyled>
